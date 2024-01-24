7028602cdc13c8207b7571488f21a7dcd7f09718
niwemu1 <niwemugenideborah@gmail.com>
2024-01-24 13:08:32 -0800

    Update My Website For Week1 and Week2

--- week01/scripts/getDates.js
+++ week01/scripts/getDates.js
@@ -21,10 +21,3 @@ myBtn.addEventListener("click", () => {
 });
 
 
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
